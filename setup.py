b58806a5ac7a73415addb9b991861e80176a8e07
Fadhly Al-farizi <fadhlyalfarizi30@gmail.com>
2024-12-23 07:24:22 +0700

    Init

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+# Define the paths to LP Solve
+include_dirs = [r"D:\TA\Test Terakhir\banget\clara-master\lpsolve"]
+library_dirs = [r"D:\TA\Test Terakhir\banget\clara-master\lpsolve"]
+libraries = ["lpsolve55"]  # Replace with the correct library name if different
+
+# Build the extension
+extensions = [
+    Extension(
+        "clara.pylpsolve",
+        ["clara/pylpsolve.pyx"],
+        include_dirs=include_dirs,
+        library_dirs=library_dirs,
+        libraries=libraries,
+    )
+]
+
+setup(
+    ext_modules=cythonize(extensions, language_level="3"),
+)
