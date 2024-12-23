8e9f5165d270906782bb8a135b2923d44b0c4c78
Fadhly Al-farizi <74035663+fadhlyal@users.noreply.github.com>
2024-12-23 01:18:11 +0000

    setup2.1

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from Cython.Build import cythonize
 # Define the paths to LP Solve
 include_dirs = [r'/workspaces/deploy-streamlit-last/lpsolve']
 library_dirs = [r'/workspaces/deploy-streamlit-last/lpsolve']
+import sys
+sys.path.append('/workspaces/deploy-streamlit-last/clara')
+
 libraries = ["lpsolve55"]  # Replace with the correct library name if different
 
 # Build the extension
---
+++ test.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.append('/workspaces/deploy-streamlit-last/clara')
+print(sys.path)
