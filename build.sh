8038f90569401e09f6490d9c0db455cdfce920b7
Fadhly Al-farizi <74035663+fadhlyal@users.noreply.github.com>
2024-12-23 12:32:51 +0000

    container4

--- build.sh
+++ build.sh
@@ -2,5 +2,14 @@
 # Install system dependencies
 apt-get update && apt-get install -y build-essential python3-dev liblpsolve55-dev
 
+# Set LD_LIBRARY_PATH for lpsolve
+if [ -d "/mount/src" ]; then
+    # Streamlit Cloud environment
+    export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+else
+    # Codespace environment
+    export LD_LIBRARY_PATH=/workspaces/deploy-streamlit-last/lpsolve:$LD_LIBRARY_PATH
+fi
+
 # Build the Cython extension
 python setup.py build_ext --inplace
